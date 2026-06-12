60d85c1f5915a83cd161bad7b6dbc141d0bcc4b6
Samridhi bhatia <samridhibhatia014@gmail.com>
2026-06-12 16:45:32 +0530

    Fix Linux case-sensitive model import

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const authMiddleware = require("../middleware/authMiddleware");
-const User = require("../models/User");
+const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 // REGISTER
 router.post("/register", async (req, res) => {
